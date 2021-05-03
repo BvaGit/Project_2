c0f29cefcb13131ba019feeeda23b3c08c3f07f4
Maryna Hrushevska <mia.grushevska@gmail.com>
2021-05-04 01:41:04 +0300

    main page dms and sort dropdown and settings popup

--- src/front/js/script.js
+++ src/front/js/script.js
@@ -1,7 +1,9 @@
 import '../scss/style.scss';
 
 import hello from '../modules/hello';
+import settingsPopup from '../modules/settingsPopup';
 
 window.addEventListener('DOMContentLoaded', ()=>{
     hello();
+    settingsPopup();
 });
--- src/front/modules/settingsPopup.js
+++ src/front/modules/settingsPopup.js
@@ -1,4 +1,8 @@
 function settingsPopup () {
-    
+    const settings = document.getElementById('settings');
+    const settingsPopup = document.getElementById('settingsPopup');
+    settings.addEventListener('click', () => {
+        settingsPopup.classList.remove('hideSettingsPopup');
+    })
 }
 export default settingsPopup;
