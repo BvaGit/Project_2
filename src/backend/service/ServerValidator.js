dcfd1adb0b28235230ae653476045100657b0916
Daniel Kubariev <danielkubariev@gmail.com>
2021-05-21 14:23:23 +0300

    small fixes

--- src/backend/service/ServerValidator.js
+++ src/backend/service/ServerValidator.js
@@ -2,7 +2,7 @@ class ServerValidator {
   static namePattern = /[A-ZА-Я]{1,20}/i
   static emailPattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
   static phonePattern = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/
-  static regAuthPattern =/[A-ZА-Я0-9]{5,20}/i
+  static regAuthPattern =/[A-Z0-9]{5,20}/i
   static validateStr(str,regexp) {
     return typeof str === 'string' && regexp.test(str)
   }
