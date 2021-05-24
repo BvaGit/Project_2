6a0787e770c1696e65b10b4c25f626036a3ac6bb
unknown <artemvakulenko42@gmail.com>
2021-05-24 10:39:07 +0300

    new search bar

--- src/front/modules/main-page/searchBar.js
+++ src/front/modules/main-page/searchBar.js
@@ -1,40 +1,71 @@
 import renderTable from "../main"
+import getRequest from "../request"
 
-// эту фукнцию вызывать после гет запроса и передавать туда массив из респонса
 
-export function searchBar(array){
-    const searchFirstname = document.getElementById("searchFirstname")
-    const searchLastname = document.getElementById("searchLastname")
-    if (!searchFirstname.value && !searchLastname.value){
-        renderTable(array)
-    }
-    if(searchFirstname.value && !searchLastname.value){
-        var newArray = []
-        array.forEach(element => {
-            if(element.fname.includes(searchFirstname.value)){
-                newArray.push(element)
-            }    
-        });
-        renderTable(newArray)
-    }
-    if(!searchFirstname.value && searchLastname.value){
-        var newArray = []
-        array.forEach(element => {
-            if(element.lname.includes(searchLastname.value)){
-                newArray.push(element)
-            }    
-        });
-        renderTable(newArray)
-    }
-    if(searchFirstname.value && searchLastname.value){
-        var newArray = []
-        array.forEach(element => {
-            if(element.lname.includes(searchLastname.value) && element.lname.includes(searchLastname.value)){
-                newArray.push(element)
-            }    
-        });
-        renderTable(newArray)
+function getDefaultPersons(nameBase){
+    getRequest(URL + nameBase + "/persons/" + localStorage.getItem("id_user"))
+        .then(res => res.json())
+        .then((data) => {
+             renderTable(data);
+         })
+        .then(() => {
+             deletePerson();
+             deleteBtnPerson();
+         })
+         .catch(() => {
+             console.log("No");
+         }); 
+}
+
+
+export function searchBar(dbms){
+
+    getRequest("http://localhost:2020/api/" + dbms + "/persons/" + localStorage.getItem("id_user"))
+        .then(res => res.json())
+        .then((data) => {
+            filterArray(data);
+         }).then(() => {
+            deletePerson();
+            deleteBtnPerson();
+        })
+        .catch(() => {
+            console.log("No");
+        }); 
+
+    function filterArray(array){
+        const searchFirstname = document.getElementById("searchFirstname")
+        const searchLastname = document.getElementById("searchLastname")
+        if (!searchFirstname.value && !searchLastname.value){
+            renderTable(array)
+        }
+        if(searchFirstname.value && !searchLastname.value){
+            var newArray = []
+            array.forEach(element => {
+                if(element.fname.includes(searchFirstname.value)){
+                    newArray.push(element)
+                }    
+            });
+            renderTable(newArray)
+        }
+        if(!searchFirstname.value && searchLastname.value){
+            var newArray = []
+            array.forEach(element => {
+                if(element.lname.includes(searchLastname.value)){
+                    newArray.push(element)
+                }    
+            });
+            renderTable(newArray)
+        }
+        if(searchFirstname.value && searchLastname.value){
+            var newArray = []
+            array.forEach(element => {
+                if(element.lname.includes(searchLastname.value) && element.lname.includes(searchLastname.value)){
+                    newArray.push(element)
+                }    
+            });
+            renderTable(newArray)
+        }
     }
 }
 
-//ещё повесить листенера на keydown у инпутов и вызывать эту функцию
+//ещё повесить листенера на change у инпутов и вызывать эту функцию
