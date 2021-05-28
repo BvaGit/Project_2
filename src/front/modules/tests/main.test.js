bfcdd7c48e33b3f910caca754be73a242c0104a9
ArtemVakulenko <artemvakulenko42@gmail.com>
2021-05-28 11:04:33 +0300

    test environment setup in front

---
+++ src/front/modules/tests/main.test.js
@@ -0,0 +1,10 @@
+import main from "../main"
+
+describe('main', function () {
+    it('should be defined', function () {
+        expect(main).toBeDefined();
+    })
+    it('should be function', function () {
+        expect(typeof main).toBe('function');
+    })
+})
