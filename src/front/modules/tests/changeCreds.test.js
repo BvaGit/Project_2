8d2704f1275acd47c33f303e77c105dc87ebe3fa
ArtemVakulenko <artemvakulenko42@gmail.com>
2021-05-31 01:49:03 +0300

    valid.js test

---
+++ src/front/modules/tests/changeCreds.test.js
@@ -0,0 +1,13 @@
+import ChangeCreds from "../main-page/changeCreds"
+
+describe('ChangeCreds', function () {
+    it('should be defined', function () {
+        expect(ChangeCreds).toBeDefined();
+    })
+    it('ChangeCreds should be function', function () {
+        expect(typeof ChangeCreds).toBe('function');
+    })
+    it('ChangeCreds should be function', function () {
+        expect(typeof ChangeCreds).toBe('function');
+    })
+})
