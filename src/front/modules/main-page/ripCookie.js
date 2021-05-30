71db2afd6501b8b28248e16b839adb0c8947d61e
Maryna Hrushevska <mia.grushevska@gmail.com>
2021-05-31 01:14:22 +0300

    rip cookie test

--- src/front/modules/main-page/ripCookie.js
+++ src/front/modules/main-page/ripCookie.js
@@ -1,4 +1,4 @@
-function getCookie(cname) {
+export function getCookie(cname) {
     var name = cname + "=";
     var ca = document.cookie.split(';');
     for(var i = 0; i < ca.length; i++) {
---
+++ src/front/modules/tests/ripCookie.test.js
@@ -0,0 +1,24 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import checkCookie from '../main-page/ripCookie'
+import { getCookie } from '../main-page/ripCookie'
+
+describe('checkCookie', function () {
+    it('checkCookie should be defined', function () {
+        expect(checkCookie).toBeDefined();
+    })
+    it('checkCookie should be function', function () {
+        expect(typeof checkCookie).toBe('function');
+    })
+})
+
+describe('getCookie', function () {
+    it('getCookie should be defined', function () {
+        expect(getCookie).toBeDefined();
+    })
+    it('getCookie should be function', function () {
+        expect(typeof getCookie).toBe('function');
+    })
+})
